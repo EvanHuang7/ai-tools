32717256adcc16ad866cd892d60b91478b268214
Zhihui Huang <zhh399@mail.usask.ca>
2025-06-14 19:54:55 -0600

    fix node service to get env variable from either .env file or a secret file

--- node-backend/src/lib/postgre.js
+++ node-backend/src/lib/postgre.js
@@ -2,6 +2,11 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "../db/schema.js";
+import fs from "fs";
 
-const client = postgres(process.env.DATABASE_URL);
+databaseUrl =
+  process.env.DATABASE_URL ||
+  fs.readFileSync(process.env.DATABASE_URL_FILE, "utf8");
+
+const client = postgres(databaseUrl);
 export const postgreDbClient = drizzle(client, { schema });
