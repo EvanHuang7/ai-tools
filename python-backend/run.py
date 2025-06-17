09a6cff2fadd3bc04b4b3184d9cd9c6067526b8d
Zhihui Huang <zhh399@mail.usask.ca>
2025-06-16 18:39:40 -0600

    Fix unhealthy container issue for go and python by update go Dockerfile and update python requirement.text file

--- python-backend/run.py
+++ python-backend/run.py
@@ -2,13 +2,18 @@ from app import create_app
 
 app = create_app()
 
-# Cli to start virtual env: source venv/bin/activate
+# Cli to start virtual env: source .venv/bin/activate
+# Cli to exit virtual env: deactivate
+
 # Cli to run app:
 # export FLASK_APP=run.py
 # export FLASK_ENV=development
 # flask run --reload --port=8088
 
-# Cli to update requirement.text after install new package: 
+# Cli to install packages in "requirement.text":
+# pip install -r ./python-backend/requirements.txt
+
+# Cli to update "requirement.text" after install new package: 
 # pip freeze > requirements.txt
 if __name__ == "__main__":
     # This allows running the app with "python run.py" as well
