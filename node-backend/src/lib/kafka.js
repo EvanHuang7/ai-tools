7b397a14ae96ffb9103234fde8cbf5d40bbe7953
Zhihui Huang <zhh399@mail.usask.ca>
2025-07-08 19:37:04 -0600

    add env varable to docker and K8s files for node serivices

--- node-backend/src/lib/kafka.js
+++ node-backend/src/lib/kafka.js
@@ -1,20 +1,19 @@
 import { Kafka } from "kafkajs";
 import {
   kafkaClientId,
-  kafkaBrokers,
   kafkaMechanism,
   kafkaUsername,
-  kafkaPassword,
 } from "../utils/constants.js";
+import { getEnvOrFile } from "../utils/utils.js";
 
 const kafkaClient = new Kafka({
   clientId: kafkaClientId,
-  brokers: kafkaBrokers,
+  brokers: [getEnvOrFile("KAFKA_BOOTSTRAP_SERVER")],
   ssl: true,
   sasl: {
     mechanism: kafkaMechanism,
     username: kafkaUsername,
-    password: kafkaPassword,
+    password: getEnvOrFile("KAFKA_SASL_USER_PASSWORD"),
   },
 });
 
