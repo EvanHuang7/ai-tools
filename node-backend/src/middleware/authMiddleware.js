a8b603ec61ae86c6ed88bb965764d535a45a65d9
Zhihui Huang <zhh399@mail.usask.ca>
2025-07-09 21:23:44 -0600

    update the authMiddleware to geet user plan in nodejs serivce

--- node-backend/src/middleware/authMiddleware.js
+++ node-backend/src/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 import { clerkClient, getAuth } from "@clerk/express";
+import jwt from "jsonwebtoken"; // built-in JWT decode
 
 export const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization || "";
@@ -10,17 +11,37 @@ export const authMiddleware = async (req, res, next) => {
   }
 
   try {
-    // Use `getAuth()` to get the user's `userId`
+    // Get userId
     const { userId } = getAuth(req);
     if (!userId) {
       return res.status(401).json({ error: "Unauthorized: No userId found" });
     }
 
-    // Use Clerk's JavaScript Backend SDK to get the user's User object
+    // Get full user info object
     const user = await clerkClient.users.getUser(userId);
 
-    // Attach full user info to the request object
+    // Decode token to get V2 JWT claims
+    const token = authHeader.replace("Bearer ", "");
+    const decoded = jwt.decode(token);
+    if (!decoded || typeof decoded !== "object") {
+      return res.status(400).json({ error: "Failed to decode token" });
+    }
+
+    // Extract user plan like "pro_user" from "u:pro_user"
+    const rawPlan = decoded.pla;
+    let userPlan = null;
+    if (typeof rawPlan === "string" && rawPlan.startsWith("u:")) {
+      // eg. "pro_user"
+      userPlan = rawPlan.split(":")[1];
+    }
+    if (!userPlan) {
+      return res.status(400).json({ error: "User plan not found in JWT" });
+    }
+
+    // Attach userId, full user info and userPlan to the request object
+    req.userId = userId;
     req.user = user;
+    req.userPlan = userPlan;
 
     next();
   } catch (error) {
