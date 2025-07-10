4900c2acf0f86e5fd32e77c542d645102b7c5803
Zhihui Huang <zhh399@mail.usask.ca>
2025-07-09 23:39:59 -0600

    add "user_month_unique" unique index to audioFeatureMonthlyUsage table for preventing duplicate rows for the same user & month

--- node-backend/src/db/schema.js
+++ node-backend/src/db/schema.js
@@ -1,4 +1,12 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  int,
+  index,
+  uniqueIndex,
+} from "drizzle-orm/pg-core";
 
 // Note: Whenever adding a new schema,
 // run "npx drizzle-kit generate" and "npx drizzle-kit migrate" clis
@@ -25,20 +33,40 @@ export const audios = pgTable("audios", {
   id: serial("id").primaryKey().notNull(), // serial will be auto-generated
   userId: text("userId").notNull(),
   topic: text("topic").notNull(),
-  audioUrl: text("audioUrl"),
+  audioUrl: text("audioUrl").notNull(),
   createdAt: timestamp("created_at", {
     withTimezone: false,
-    mode: "date", // important
+    mode: "date",
+  }).defaultNow(),
+  updatedAt: timestamp("updated_at", {
+    withTimezone: false,
+    mode: "date",
   }).defaultNow(),
 });
 
-export const audioFeatureMonthlyUsage = pgTable("audioFeatureMonthlyUsage", {
-  id: serial("id").primaryKey().notNull(), // serial will be auto-generated
-  userId: text("userId").notNull(),
-  usage: int("usage").notNull(),
-  monthAndYear: timestamp("monthAndYear"),
-  createdAt: timestamp("created_at", {
-    withTimezone: false,
-    mode: "date", // important
-  }).defaultNow(),
-});
+export const audioFeatureMonthlyUsage = pgTable(
+  "audioFeatureMonthlyUsage",
+  {
+    id: serial("id").primaryKey().notNull(),
+    userId: text("userId").notNull(),
+    usage: int("usage").notNull(),
+    monthAndYear: timestamp("monthAndYear", {
+      withTimezone: false,
+      mode: "date",
+    }).notNull(),
+    createdAt: timestamp("created_at", {
+      withTimezone: false,
+      mode: "date",
+    }).defaultNow(),
+    updatedAt: timestamp("updated_at", {
+      withTimezone: false,
+      mode: "date",
+    }).defaultNow(),
+  },
+  (table) => ({
+    userMonthUnique: uniqueIndex("user_month_unique").on(
+      table.userId,
+      table.monthAndYear
+    ),
+  })
+);
