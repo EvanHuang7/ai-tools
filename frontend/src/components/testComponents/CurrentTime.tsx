be1447e4d503fcdec46392e80dae6ea9f512fe69
Zhihui Huang <zhh399@mail.usask.ca>
2025-07-09 21:34:27 -0600

    update frontend to get user billing subscriptions info

--- frontend/src/components/testComponents/CurrentTime.tsx
+++ frontend/src/components/testComponents/CurrentTime.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useCurrentTime } from "../../api/currentTime/currentTime.queries";
+import { useClerk } from "@clerk/clerk-react";
 
 interface CurrentTimeProps {
   api: string;
@@ -7,6 +8,23 @@ interface CurrentTimeProps {
 
 const CurrentTime: React.FC<CurrentTimeProps> = ({ api }) => {
   const { data, isLoading, isError, error, isFetching } = useCurrentTime(api);
+
+  // Test log for getting billing subscriptions info in frontend
+  const { billing } = useClerk();
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const result = await billing.getSubscriptions({
+          initialPage: 1,
+          pageSize: 10,
+        });
+        console.log("✅ subscriptionsResult in front-end", result);
+      } catch (err) {
+        console.error("❌ Failed to fetch subscriptions", err);
+      }
+    };
+    fetch();
+  }, [billing]); // include billing in deps
 
   if (isLoading) return <p>Loading {api}...</p>;
   if (isError) return <p>Error: {error?.message}</p>;
