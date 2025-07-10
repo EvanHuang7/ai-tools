83e1c38042ed1c5a54b61355a5213ce20da21963
Zhihui Huang <zhh399@mail.usask.ca>
2025-07-09 22:41:13 -0600

    add IncrementImageFeatureMonthlyUsage service function

--- go-backend/service/imageMonthlyUsage.go
+++ go-backend/service/imageMonthlyUsage.go
@@ -14,10 +14,12 @@ import (
 func GetImageFeatureMonthlyUsage(userID string) (int, error) {
     monthStart := utils.GetCurrentYearAndMonthStart(time.Now())
 
+	// Check current usage record
     var usageRecord db.ImageFeatureMonthlyUsage
     err :=  db.DB.Where("user_id = ? AND month_and_year = ?", userID, monthStart).
         First(&usageRecord).Error
 
+	// If no record found or get data query errors, return 0
     if errors.Is(err, gorm.ErrRecordNotFound) {
         return 0, nil
     }
@@ -29,3 +31,27 @@ func GetImageFeatureMonthlyUsage(userID string) (int, error) {
 }
 
 // Increase user image feature monthly usage or create a record if no exsit
+func IncrementImageFeatureMonthlyUsage(userID string, count int) error {
+    monthStart := utils.GetCurrentYearAndMonthStart(time.Now())
+
+	// Check current usage record
+    var usageRecord db.ImageFeatureMonthlyUsage
+    err := db.DB.Where("user_id = ? AND month_and_year = ?", userID, monthStart).
+        First(&usageRecord).Error
+
+	// Create a record if not exist
+    if errors.Is(err, gorm.ErrRecordNotFound) {
+        usageRecord = db.ImageFeatureMonthlyUsage{
+            UserID:       userID,
+            Usage:        count,
+            MonthAndYear: monthStart,
+        }
+        return db.DB.Create(&usageRecord).Error
+    } else if err != nil {
+        return err
+    }
+
+    // Increment usage if exist
+    usageRecord.Usage += count
+    return db.DB.Save(&usageRecord).Error
+}
