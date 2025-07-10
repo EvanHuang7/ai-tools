7694ea336b45dbf3e304c5c1be4bd981d7e605b2
Zhihui Huang <zhh399@mail.usask.ca>
2025-07-10 16:28:39 -0600

    fix the rediskey generaation include correct current timestamp in GetAppMonthlyUsageKey grpc

--- go-backend/grpc/appService.go
+++ go-backend/grpc/appService.go
@@ -13,8 +13,13 @@ type appServer struct {
 }
 
 func (s *appServer) GetAppMonthlyUsageKey(ctx context.Context, req *apppb.GetAppMonthlyUsageKeyRequest) (*apppb.GetAppMonthlyUsageKeyResponse, error) {
-	// Sample logic: generate redis key like "usage:<userId>:2025-07"
-	redisKey := fmt.Sprintf("usage:%s:%s", req.UserId, time.Now().Format("2006-01"))
+	// Step 1: Generate a key
+	// Sample logic: generate redis key like "usage:abc123:2025-07-10-22-49-33"
+	redisKey := fmt.Sprintf("usage:%s:%s", req.UserId, time.Now().Format("2006-01-02-15-04-05"))
+
+	// Step 2: Get the video and remove bg image features usage
+
+	// Step 3: Send a pubsub message with all info to Node backend
 	return &apppb.GetAppMonthlyUsageKeyResponse{
 		RedisKey: redisKey,
 	}, nil
