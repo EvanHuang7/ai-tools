04479326e1b3bf9a49136c1924e417aa75ad77ae
Zhihui Huang <zhh399@mail.usask.ca>
2025-07-11 00:09:06 -0600

    enable git workflow for testing GKE cluster

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -11,7 +11,7 @@ import "../App.css";
 export function TestPage() {
   return (
     <div>
-      <h1>AI Tools App 👋 Test in GCP VM</h1>
+      <h1>AI Tools App 👋 Test in GKE cluster</h1>
       <CurrentTime api="/api/node/" />
       <CurrentTime api="/api/go/" />
       <CurrentTime api="/api/python/" />
