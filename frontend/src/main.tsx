bd295aa17c259eb76f091eb141c937e58d7da5e1
Zhihui Huang <zhh399@mail.usask.ca>
2025-07-18 21:23:04 -0600

    add UserPlanProvider to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { UserPlanProvider } from "./contexts/UserPlanContext.tsx";
 import App from "./App.tsx";
 import "./index.css";
 import queryClient from "./lib/queryClient.ts";
@@ -16,11 +17,13 @@ if (!clerkPubKey) {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={clerkPubKey}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
+      <UserPlanProvider>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </UserPlanProvider>
     </ClerkProvider>
   </React.StrictMode>
 );
