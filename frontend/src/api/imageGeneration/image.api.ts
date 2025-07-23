e1df53efae55c4b58784ffff436059652bafc7a8
Zhihui Huang <zhh399@mail.usask.ca>
2025-07-22 21:15:03 -0600

    update image generation api file

--- frontend/src/api/imageGeneration/image.api.ts
+++ frontend/src/api/imageGeneration/image.api.ts
@@ -1,15 +1,21 @@
 import type { AxiosInstance } from "axios";
-import type { GenerateImageRequest } from "@/types/api";
+import type {
+  GenerateImageRequest,
+  GenerateImageResponse,
+  ListImagesResponse,
+} from "@/types/api";
 
 export const generateImage = async (
   data: GenerateImageRequest,
   axios: AxiosInstance
-) => {
+): Promise<GenerateImageResponse> => {
   const response = await axios.post("/api/go/generate-image", data);
   return response.data;
 };
 
-export const listImages = async (axios: AxiosInstance) => {
+export const listImages = async (
+  axios: AxiosInstance
+): Promise<ListImagesResponse> => {
   const response = await axios.get("/api/go/list-images");
   return response.data;
 };
