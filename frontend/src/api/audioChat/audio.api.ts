e4b54f6aec6d5c69ff2743853300fc8e44f1f9f5
Zhihui Huang <zhh399@mail.usask.ca>
2025-07-22 21:15:24 -0600

    update audio chat api file

--- frontend/src/api/audioChat/audio.api.ts
+++ frontend/src/api/audioChat/audio.api.ts
@@ -1,7 +1,14 @@
 import type { AxiosInstance } from "axios";
-import type { CreateAudioRequest } from "@/types/api";
+import type {
+  CreateAudioRequest,
+  CreateAudioResponse,
+  ListAudiosResponse,
+  StartAudioResponse,
+} from "@/types/api";
 
-export const startAudio = async (axios: AxiosInstance) => {
+export const startAudio = async (
+  axios: AxiosInstance
+): Promise<StartAudioResponse> => {
   const response = await axios.get("/api/node/audio/start");
   return response.data;
 };
@@ -9,12 +16,14 @@ export const startAudio = async (axios: AxiosInstance) => {
 export const createAudio = async (
   data: CreateAudioRequest,
   axios: AxiosInstance
-) => {
+): Promise<CreateAudioResponse> => {
   const response = await axios.post("/api/node/audio/create", data);
   return response.data;
 };
 
-export const listAudios = async (axios: AxiosInstance) => {
+export const listAudios = async (
+  axios: AxiosInstance
+): Promise<ListAudiosResponse> => {
   const response = await axios.get("/api/node/audio/list");
   return response.data;
 };
