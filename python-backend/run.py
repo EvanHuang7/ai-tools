06d99d3be5253a3263aa0ab9ae3ff7cb6dafb1ce
Zhihui Huang <zhh399@mail.usask.ca>
2025-08-01 23:31:52 -0600

    add more python virtual env cli

--- python-backend/run.py
+++ python-backend/run.py
@@ -2,18 +2,19 @@ from app import create_app
 
 app = create_app()
 
-# Cli to start virtual env in "ROOT FOLDER": source .venv/bin/activate
-# Cli to exit virtual env: deactivate
+# - Cli to create a new virtual env in "ROOT FOLDER": python3 -m venv .venv
+# - Cli to activate virtual env in "ROOT FOLDER": source .venv/bin/activate
+# - Cli to exit virtual env: deactivate
 
-# Cli to run app in "PYTHON PROJECT FOLDER":
+# - Cli to run app in "PYTHON PROJECT FOLDER":
 # export FLASK_APP=run.py
 # export FLASK_ENV=development
 # flask run --reload --port=8088
 
-# Cli to install packages in "requirement.text":
+# - Cli to install packages in virtual env from "requirement.text" file:
 # pip install -r ./python-backend/requirements.txt
 
-# Cli to update "requirement.text" after install new package: 
+# - Cli to update packages list in "requirement.text" after install new package: 
 # pip freeze > requirements.txt
 if __name__ == "__main__":
     # This allows running the app with "python run.py" as well.
